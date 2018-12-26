58efd13f14b3c8b062e1f810705703057189caf5
15338066274 <401934990@qq.com>
2018-12-26 23:51:28 +0800

    修改后提交

--- HelloWorld/src/main/java/org/gls/HelloWorld/HelloWorldApplication.java
+++ HelloWorld/src/main/java/org/gls/HelloWorld/HelloWorldApplication.java
@@ -2,7 +2,7 @@ package org.gls.HelloWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//789789789
 @SpringBootApplication
 public class HelloWorldApplication {
 
