87972370f4dc3c8570b6a6bb065370147729dee5
15338066274 <35292371+15338066274@users.noreply.github.com>
2018-12-27 01:16:14 +0800

    Update HelloWorldApplication.java

--- HelloWorld/src/main/java/org/gls/HelloWorld/HelloWorldApplication.java
+++ HelloWorld/src/main/java/org/gls/HelloWorld/HelloWorldApplication.java
@@ -2,7 +2,7 @@ package org.gls.HelloWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//12312 aaaaaaa
+//aaa
 @SpringBootApplication
 public class HelloWorldApplication {
 
